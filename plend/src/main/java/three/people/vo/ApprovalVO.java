8ce4878a293d7359100def1ba18dd1f7a4a1a5a1
chongmin <92002849+seohakman@users.noreply.github.com>
2022-07-02 12:30:13 +0900

    VO test

---
+++ plend/src/main/java/three/people/vo/ApprovalVO.java
@@ -0,0 +1,13 @@
+package three.people.vo;
+
+public class ApprovalVO extends BookVO {
+	
+	private int apidx;
+	private int assort;
+	private String category;
+	private String reason;
+	private String date;
+	
+	
+	
+}
