542fbbce40fb8c7c3ae3dc9ed877165f359895e3
APplE <rh40614@naver.com>
2022-07-04 11:56:30 +0900

    Create CommonController.java

---
+++ plend/src/main/java/three/people/controller/CommonController.java
@@ -0,0 +1,9 @@
+package three.people.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonController {
+
+}
