5aa6995a40f6ebbd4596b0e7d0d15fdb978ed97b
chongmin <92002849+seohakman@users.noreply.github.com>
2022-07-04 09:44:22 +0900

    VO

---
+++ plend/src/main/java/three/people/vo/HeartVO.java
@@ -0,0 +1,14 @@
+package three.people.vo;
+
+public class HeartVO extends PlaceVO{
+
+	private int hidx;
+
+	public int getHidx() {
+		return hidx;
+	}
+
+	public void setHidx(int hidx) {
+		this.hidx = hidx;
+	}
+}
