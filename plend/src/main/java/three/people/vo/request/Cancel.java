e454d8e3d0cfccca08f5dfd65961c181f0a81cec
APplE <rh40614@naver.com>
2022-07-29 17:45:24 +0900

    [KYH] 필터
    [KYH] 필터

--- plend/src/main/java/three/people/vo/request/Cancel.java
+++
@@ -1,9 +0,0 @@
-package three.people.vo.request;
-
-public class Cancel {
-    public String receiptId;
-    public Double price;
-    public String name;
-    public String reason;
-    public RefundData refund;
-}
