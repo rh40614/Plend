f9da58283ec662288d8de701676cb1877d4ac5ca
chongmin <dudals1545@naver.com>
2022-08-24 10:41:21 +0900

    [KYM] searchService 생성
    기초 페이지와 한 페이지당 보여줄 페이지 계산

--- plend/src/main/java/three/people/controller/CommonController.java
+++ plend/src/main/java/three/people/controller/CommonController.java
@@ -258,9 +258,6 @@ public class CommonController  {
 			login.setRole(user.getRole());
 			login.setNickName(user.getNickName());
 			
-			//System.out.println("role ="+user.getRole());
-			//System.out.println("nickname = "+user.getNickName());
-			
 			//자동 로그아웃 시간 30분
 			//움직이지 않고 가만히 있을 경우 식ㄴ이 흘러 30분이 경과됐을 때 자동 로그아웃
 			session.setMaxInactiveInterval(1800);
@@ -356,9 +353,6 @@ public class CommonController  {
 			login.setRole(user.getRole());
 			login.setNickName(user.getNickName());
 			
-			//System.out.println("role = "+user.getRole());
-			//System.out.println("nickname = "+user.getNickName());
-
 			session.setAttribute("login", login);
 			return "redirect:/";
 
